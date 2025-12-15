33d001fbc9e096c27f7f52008b9330495cca1548
Christian Lapinig <clapinig@adobe.com>
2025-12-15 06:41:08 -0800

    complete day 3 part 1

---
+++ 2025/day3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"day3/part1"
+)
+
+func main() {
+	fmt.Println("Advent of Code - 2025 - Day 3: Lobby")
+	if len(os.Args) < 2 {
+		fmt.Println("Path to input file required.")
+		fmt.Println("Usage: go run main.go <input-file>")
+		os.Exit(1)
+	}
+
+	filePath := os.Args[1]
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("error reading file %v", err)
+	}
+
+	result1, err := part1.GetMaxVoltage(content)
+	if err != nil {
+		log.Fatalf("error calculating result for part 1: %v", err)
+	}
+	fmt.Println("Part 1 answer: ", result1)
+}
